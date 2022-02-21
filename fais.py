bc64e0d8b80174b59ec90198a82d8c1c06fffae8
Faisst <99186848+Faisst@users.noreply.github.com>
2022-02-21 21:06:20 +0700

    Add files via upload

---
+++ fais.py
@@ -0,0 +1,14 @@
+#!/bin/bash
+clear
+
+figlet -f slant "mr fais gg"
+
+<<EOF
+
+qu="selamt datang"
+
+
+        read -p "[•] Masukan Nama: " faiss
+        read -p "[•] Masukan pass: " store
+        sleep 1; echo
+
